3bb263124db5e274431c69af997c824abdc850b4
thayron <devthayronhiglander@gmail.com>
2025-05-16 18:02:20 -0300

    tudo ok

--- agendamento_carros/settings.py
+++ agendamento_carros/settings.py
@@ -84,20 +84,20 @@ WSGI_APPLICATION = 'agendamento_carros.wsgi.application'
 
 # SQLLITE
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
-#     'default': dj_database_url.parse(
-#         os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
-#         conn_max_age=600
-#     ),
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        conn_max_age=600
+    ),
+}
 
 
 # mySQL
