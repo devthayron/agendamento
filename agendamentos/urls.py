e511e969bda456ee8445e081296ce3cc63345bd8
thayron <devthayronhiglander@gmail.com>
2025-05-16 17:14:30 -0300

    comentando o codigo do pdf

--- agendamentos/urls.py
+++ agendamentos/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('exportar-csv/', views.exportar_agendamentos_csv, name='exportar_agendamentos_csv'),
     path('agendamentos/', views.painel_users_list, name='painel_user'),  
     path('agendamentos/editar/<int:id>/', views.agendamento_editar, name='agendamento_editar'),
-    path('agendamento/<int:agendamento_id>/pdf/', views.baixar_agendamento_pdf, name='baixar_agendamento_pdf'),
+#     path('agendamento/<int:agendamento_id>/pdf/', views.baixar_agendamento_pdf, name='baixar_agendamento_pdf'),
 ]
