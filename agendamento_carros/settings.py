79ad2629a33528be8a2c1a094e96fc54150fc952
thayron <devthayronhiglander@gmail.com>
2025-05-16 18:31:55 -0300

    testand

--- agendamento_carros/settings.py
+++ agendamento_carros/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY','chave-secreta-temporaria-para-dev') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*","https://agendamento-tdc.up.railway.app"]
 
